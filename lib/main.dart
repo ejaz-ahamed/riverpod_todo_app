1ec33f6bab9011c3fcbcd928f7ada71467a2481a
ejaz-ahamed <ahamedejaz896@gmail.com>
2023-11-15 11:55:35 +0530

    main page changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:state_management/view/learn%20Api/api_homepage_provider.dart';
-import 'package:state_management/view/learn%20Api/api_integration.dart';
+// import 'package:state_management/view/learn%20Api/api_integration.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
